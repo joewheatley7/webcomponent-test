9fd2a7234af2fd4585feeb1d64afcf61cfd29b01
Joe Wheatley <joe.wheatley@causeway.com>
2020-07-27 17:57:01 +0100

    Made webcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portal-web-component-app';
+export class AppComponent implements OnChanges {
+
+  @Input()
+  title: string
+
+  @Input()
+  message: any
+
+  @Output()
+  api = new EventEmitter();
+
+  ngOnChanges(simpleChanges: SimpleChanges) {
+    if (simpleChanges) {
+      this.api.emit(`${this.title} : recieved ${simpleChanges.message.currentValue}`);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,4 +13,10 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('webcomponent-app', myElement);
+  }
+  ngDoBootstrap(): void { }
+}
